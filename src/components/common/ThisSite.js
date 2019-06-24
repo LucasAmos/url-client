b09e1ddb0f2a617a69832e952e5ec777472678ae
Lucas <ldjamos@icloud.com>
2019-06-24 07:52:53 +0100

    updates

--- src/components/common/ThisSite.js
+++ src/components/common/ThisSite.js
@@ -10,17 +10,7 @@ export default function ThisSite() {
         <Row>
           <Col className="about-col">
             <h2>About this site</h2>
-            <p>This site is built using a MERN stack (Mongo, Express, React, Node)
-              and conforms to the
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://jamstack.org/"
-              >
-                &nbsp;JAMstack&nbsp;
-              </a>
-              architecture specification.
-            </p>
+            <p>This site is built using a MERN stack (Mongo, Express, React, Node).</p>
             <p>
               Check out the
               <a

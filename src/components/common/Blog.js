d8446dc9d00cf55a5fa0449dbf9b96a347305420
Lucas <ldja@st-andrews.ac.uk>
2020-04-22 18:37:32 +0100

    style tweaks and date

--- src/components/common/Blog.js
+++ src/components/common/Blog.js
@@ -8,10 +8,11 @@ const Blog = () => {
     <NavigationWrapper>
       <div style={{ height: 50 }} />
       <BlogPreview
-        title="Use AWS Rekognition to automate your image analysis"
+        title="Use AWS Rekognition to automate your image analysis: Part 1"
         image="https://lucas-blog-images.s3.eu-west-2.amazonaws.com/rekognition.jpg"
         body="An off the shelf machine learning solution without the need to train your own models"
         link="/blog/rekognition"
+        date="22/4/2020"
       />
       <div style={{ height: 50 }} />
     </NavigationWrapper>
--- src/components/common/BlogPreview.js
+++ src/components/common/BlogPreview.js
@@ -3,7 +3,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './BlogPreview.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
-export default function BlogPreview({ title, image, body, link }) {
+export default function BlogPreview({ title, image, body, link, date }) {
   return (
     <Container className="blogPreview">
       <LinkContainer to={link}>
@@ -11,9 +11,16 @@ export default function BlogPreview({ title, image, body, link }) {
           <Col lg={4}>
             <img alt="blog" src={image} />
           </Col>
-          <Col lg={8}>
-            <h1> {title}</h1>
-            <h2>{body}</h2>
+          <Col lg={8} className="details">
+            <div id="date">
+              <h3>{date}</h3>
+            </div>
+            <div>
+              <h1> {title}</h1>
+            </div>
+            <div>
+              <h2>{body}</h2>
+            </div>
           </Col>
         </Row>
       </LinkContainer>
